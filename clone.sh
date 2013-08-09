49af6c173d419b65d7b0294dd342d343141f8a2b
Alexey Kuznetsov <axet@me.com>
2013-08-09 14:57:45 +0400

    add configure params

--- clone.sh
+++ clone.sh
@@ -2,5 +2,5 @@
 
 if [ ! -e ../protobuf-2.5.0 ]; then
   svn co http://protobuf.googlecode.com/svn/tags/2.5.0 ../protobuf-2.5.0 || exit 1
-  cd ../protobuf-2.5.0 && ./autogen.sh && ./configure || exit 1
+  cd ../protobuf-2.5.0 && ./autogen.sh && ./configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/../protobuf || exit 1
 fi
