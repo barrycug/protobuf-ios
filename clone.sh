32c1c7d8307d277d15657be3aa3b1c4de534a588
Alexey Kuznetsov <axet@me.com>
2013-08-10 00:06:44 +0400

    set clang

--- clone.sh
+++ clone.sh
@@ -3,15 +3,15 @@
 TARGET=../protobuf/
 PROTOBUFSRC=../protobuf-2.5.0/
 
-if [ ! -e "$PROTOBUFSRC" ]; then
+if [ ! -e $PROTOBUFSRC ]; then
   svn co http://protobuf.googlecode.com/svn/tags/2.5.0 $PROTOBUFSRC || exit 1
 fi
 
-if [ ! -e "$PROTOBUFSRC/configure" ]; then
-  cd "$PROTOBUFSRC" && ./autogen.sh
+if [ ! -e $PROTOBUFSRC/configure ]; then
+  cd $PROTOBUFSRC && ./autogen.sh
 fi
 
-if [ ! -e "$PROTOBUF" ]; then
-  cd $PROTOBUFSRC && ./configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/$TARGET || exit 1
+if [ ! -e $TARGET ]; then
+  cd $PROTOBUFSRC && ./configure CC="clang -std=gnu99" CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/$TARGET || exit 1
   cd $PROTOBUFSRC && make install || exit 1
 fi
