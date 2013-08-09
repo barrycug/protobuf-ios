586c651c2c386e96848d1567e4a3985bd919faee
Alexey Kuznetsov <axet@me.com>
2013-08-09 15:05:51 +0400

    separate calls

--- clone.sh
+++ clone.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-if [ ! -e ../protobuf-2.5.0 ]; then
+PROTOBUF=../protobuf/
+PROTOBUFSRC=../protobuf-2.5.0/
+
+if [ ! -e "$PROTOBUFSRC" ]; then
   svn co http://protobuf.googlecode.com/svn/tags/2.5.0 ../protobuf-2.5.0 || exit 1
-  cd ../protobuf-2.5.0 && ./autogen.sh && ./configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/../protobuf || exit 1
+fi
+
+if [ ! -e "$PROTOBUFSRC/configure" ]; then
+  cd "$PROTOBUFSRC" && ./autogen.sh
+fi
+
+if [ ! -e "$PROTOBUF" ]; then
+	cd $PROTOBUFSRC && ./configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/$PROTOBUF || exit 1
+	cd $PROTOBUFSRC && make install || exit 1
 fi
