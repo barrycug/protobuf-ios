92f4bf58141d75f8192dcc4b5549635f5cb728e1
Alexey Kuznetsov <axet@me.com>
2013-08-09 23:24:49 +0400

    fix TARGET

--- clone.sh
+++ clone.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PROTOBUF=../protobuf/
+TARGET=../protobuf/
 PROTOBUFSRC=../protobuf-2.5.0/
 
 if [ ! -e "$PROTOBUFSRC" ]; then
-  svn co http://protobuf.googlecode.com/svn/tags/2.5.0 ../protobuf-2.5.0 || exit 1
+  svn co http://protobuf.googlecode.com/svn/tags/2.5.0 $PROTOBUFSRC || exit 1
 fi
 
 if [ ! -e "$PROTOBUFSRC/configure" ]; then
@@ -12,6 +12,6 @@ if [ ! -e "$PROTOBUFSRC/configure" ]; then
 fi
 
 if [ ! -e "$PROTOBUF" ]; then
-	cd $PROTOBUFSRC && ./configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/$PROTOBUF || exit 1
+	cd $PROTOBUFSRC && ./configure CC=clang CXX="clang++ -std=c++11 -stdlib=libc++" CXXFLAGS="-O3" --prefix=$(pwd)/$TARGET || exit 1
 	cd $PROTOBUFSRC && make install || exit 1
 fi
